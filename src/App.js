ea0c78f23d62e27a60b6179130af7fb9eb6dc34d
rcmthdyt <115938724+rcmthdyt@users.noreply.github.com>
2023-02-18 21:00:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor (dataCurrencyfreaks){
+        super (dataCurrencyfreaks)
+        this.state = {
+            post: [],
+        }
+      }
+    componentDidMount (){
+        fetch('https://api.currencyfreaks.com/latest?apikey=11947ca622a8479a8ed62719a28d4121&symbols=CAD,IDR,JPY,CHF,EUR,USD,GBP')
+        .then((response) => response.json())
+        .then((result) => {            
+                this.setState ({
+                post: result})
+                                              
+                                
+                let {date, base, rates} = result               
+                const currency = Object.keys(rates)
+                .map(currency => ({
+                  rates
+                }))
+
+                console.table(Object.keys(rates))
+                
+                const CAD = rates[0] 
+                const IDR = rates[1]
+                const JPY = rates[2]
+                const CHF = rates[3]
+                const EUR = rates[4]
+                const USD = rates[5]
+                const GBP = rates[6]
+                
+                              
+                document.getElementById('date').textContent = date;
+                document.getElementById('base').textContent = base;
+                document.getElementById('rates').textContent = rates;
+                   
+        })
+    }
+        
+    render() {
+      // const listItems = rates.map((d) => <li key={d.name}>{d.name}</li>);
+      return (
+        <div>
+          
+          <h1>Currecy Changes</h1>
+          
+          <p>date: <span id="date"></span></p>
+          <p>base: <span id="base"></span></p>
+          <p>rates: <span id="rates"></span></p>
+          
+          
+        </div>
+      )
+    }
+  }
 
 export default App;
