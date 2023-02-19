f2755afd07b5a12eb8e3c432f0020fae824d78fe
rcmthdyt <115938724+rcmthdyt@users.noreply.github.com>
2023-02-19 15:36:11 +0800

    update2

--- src/App.js
+++ src/App.js
@@ -9,52 +9,51 @@ class App extends React.Component {
         }
       }
     componentDidMount (){
-        fetch('https://api.currencyfreaks.com/latest?apikey=85ad4dbd1ca245e0bf1c06440d1cfc6b&symbols=CAD,IDR,JPY,CHF,EUR,USD,GBP')
+        fetch('holdhttps://api.currencyfreaks.com/latest?apikey=85ad4dbd1ca245e0bf1c06440d1cfc6b&symbols=CAD,IDR,JPY,CHF,EUR,USD,GBP')
         .then((response) => response.json())
         .then((result) => {            
-                this.setState ({
-                post: result})
-                                              
-                                
-                let {date, base, rates} = result               
-                const currency = Object.keys(rates)
-                .map(currency => ({
-                  rates
-                }))
-
-                console.table(Object.keys(rates))
+                this.setState ({post: result})                
                 
-                const CAD = rates[0] 
-                const IDR = rates[1]
-                const JPY = rates[2]
-                const CHF = rates[3]
-                const EUR = rates[4]
-                const USD = rates[5]
-                const GBP = rates[6]
+                let {date, base, rates} = result             
                 
+                console.table (rates)               
                               
                 document.getElementById('date').textContent = date;
                 document.getElementById('base').textContent = base;
-                document.getElementById('rates').textContent = rates;
-                   
+                document.getElementById('rates.CAD').textContent = rates.CAD;
+                document.getElementById('rates.IDR').textContent = rates.IDR;
+                document.getElementById('rates.JPY').textContent = rates.JPY;
+                document.getElementById('rates.CHF').textContent = rates.CHF;
+                document.getElementById('rates.EUR').textContent = rates.EUR;
+                document.getElementById('rates.USD').textContent = rates.USD;
+                document.getElementById('rates.GBP').textContent = rates.GBP;                
         })
     }
-        
     render() {
-      // const listItems = rates.map((d) => <li key={d.name}>{d.name}</li>);
       return (
+        
         <div>
           
           <h1>Currecy Changes</h1>
-          
-          <p>date: <span id="date"></span></p>
-          <p>base: <span id="base"></span></p>
-          <p>rates: <span id="rates"></span></p>
-          
-          
+          <p>Tanggal: <span id="date"></span></p>
+          <p>Nilai Tukar: <span id="base"></span></p>
+          <h2>Daftar Mata Uang</h2>
+
+            <ul>
+            <p>CAD: <span id="rates.CAD"></span></p>
+            <p>IDR: <span id="rates.IDR"></span></p>
+            <p>JPY: <span id="rates.JPY"></span></p>
+            <p>CHF: <span id="rates.CHF"></span></p>
+            <p>EUR: <span id="rates.EUR"></span></p>
+            <p>USD: <span id="rates.USD"></span></p>
+            <p>GBP: <span id="rates.GBP"></span></p>
+            </ul>
+
         </div>
       )
     }
+        
+    
   }
 
 export default App;
